db323c8ab1f2e6ad6104a233566b5698ec2c90b8
chu23465 <130033130+chu23465@users.noreply.github.com>
2024-07-22 18:23:52 +0000

    Update utils.py

--- tgcf/utils.py
+++ tgcf/utils.py
@@ -30,15 +30,17 @@ def platform_info():
     \n{platform.architecture()} {platform.processor()}"""
 
 async def checkIfForum(
-    channelid: int
+    channelid: int, tm: "TgcfMessage"
     ) -> bool:
+    client: TelegramClient = tm.client
     channelEntity = await client.get_entity(channelid)
     # return should always be a bool
     return channelEntity.forum
 
 async def getTopicIDs(
-    channelid: int
+    channelid: int, tm: "TgcfMessage"
     ) -> List[int]:
+    client: TelegramClient = tm.client
     topicIDs = []
     forumsList = await client(GetForumTopicsRequest(
         channelid,
